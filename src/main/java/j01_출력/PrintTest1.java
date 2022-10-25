1ea27c3d94ee53cd8c3aadaab9d8231d8ed7f6f6
chfhr0 <empathy_0@naver.com>
2022-10-25 21:10:51 +0900

    chapter1 - 출력

---
+++ src/main/java/j01_출력/PrintTest1.java
@@ -0,0 +1,30 @@
+package j01_출력;
+
+
+import static java.time.Clock.system;
+
+public class PrintTest1 {
+    
+    public static void main(String[] args) {
+        // 한 줄 주석
+        
+        /*
+        여러 줄 주석
+         */
+        
+        /**
+         * 클래스 또는 메소드의 역할을 명시 또는 설명하기 위한 주석
+         */
+
+        // 문자열 출력r
+        System.out.print("이름: "); // "" 큰따옴표 안에 \n을 하면 줄이 바뀐다. System.out.print("이름\n")
+        System.out.println("전병욱");
+        System.out.print("주소: ");
+        System.out.println("부산 동래구 사직동");
+
+
+        boolean isUsernameCheckFlag = true; // alt + shift + u = 다른 표기법으로 바뀐다.
+
+    }
+
+}
